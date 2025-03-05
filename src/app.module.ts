e9fd37060c2a93207b6be8ebfdf0351091a6a64a
Divyansh Jaiswal <divyanshjais8@gmail.com>
2025-03-05 10:05:08 +0530

    Basic modules setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BookmarkModule],
+  controllers: [],
+  providers: [AuthService],
 })
 export class AppModule {}
