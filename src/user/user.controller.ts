0148cc2fd0aae4edae23e695fc7503a980655345
Divyansh Jaiswal <divyanshjais8@gmail.com>
2025-03-12 10:12:42 +0530

    Cutom JWT guard and get user decorator updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,16 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/decorators';
-import { Prisma } from '@prisma/client';
+import { JwtGuard } from 'src/auth/guards';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
   constructor(private authService: UserService) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMyInfo(@GetUser() user: any) {
+  getMyInfo(@GetUser() user: User) {
     return user;
   }
 }
